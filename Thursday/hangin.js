79522c43df4c25318ba214d72c739d4972796e54
Brian Elliston <brianelliston@Brians-MacBook-Pro.local>
2018-01-11 15:04:08 -0500

    Push

--- Thursday/hangin.js
+++ Thursday/hangin.js
@@ -3,7 +3,7 @@ var words = require("./words.js")
 var inquirer = require('inquirer');
 //This is where I am a variable to pass in later
 var createdWord = new Word();
-
+var randomWord;
 var questions = [{
 name: "letter_guessed",
 message: "Please guess a letter"
@@ -19,10 +19,11 @@ function gameStarter(){
    )
    .then(function(data){
      
-     createdWord.bobContains(data.letter_guessed);
+     var result = createdWord.bobContains(data.letter_guessed);
+     console.log(result);
      createdWord.displayWord();
      //still need to check if we are done
-     if (word.finished()){
+     if (createdWord.finished()){
        return
      }
      gameStarter();
@@ -36,7 +37,14 @@ function Word(randomWord){
     
    randomWord = words[Math.floor(Math.random() * words.length)];
     
-    this.bobContains = function(letter){}
+    this.bobContains = function(letter){  // Checks if typed letter is in the word to be guessed or not
+      if (this.indexOf(letter) < 0){
+        return false;
+      }
+      else{
+        return true;
+      }
+    }
     // this.displayWord = function(){} need this for this.finished
     this.lettersArray = [];
     for (var i = 0; i < randomWord.length; i++){
@@ -44,11 +52,15 @@ function Word(randomWord){
       this.lettersArray.push(new Letter(randomWord[i]));
     }
     this.finished = function(){
-      
+      console.log("hello");
     }
     this.displayWord = function() {
       // iterate through the Letters in our array
-      
+      // for (var i = 0; i < randomWord.length; i++){
+        
+      //   if ()
+      // }
+      console.log("hello 2");
       
       // call .display() method on each of our Letters
       // put them all together, with spaces in between
